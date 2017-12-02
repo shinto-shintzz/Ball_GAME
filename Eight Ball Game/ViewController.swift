e17c4028412986c01e2b49755db143e3a47f4fc8
Shinto Joseph <shintojoseph@Shintos-MacBook-Air.local>
2017-12-02 23:01:20 +0530

    First Commit

--- Eight Ball Game/ViewController.swift
+++ Eight Ball Game/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    var randomBallNumber = 0
 
+    @IBOutlet weak var ballImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //updateBallImage()
+      
     }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func askButtonTapped(_ sender: Any) {
+        
+        updateBallImage()
+        
+        
     }
-
+    
+    func updateBallImage(){
+        
+        randomBallNumber = Int(arc4random_uniform(4))
+        ballImage.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateBallImage()
+    }
+   
 
 }
 
